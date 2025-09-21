b5802d63f9c4e54723c185883d89394f8b9d76ab
IshanIITBBS <137220069+IshanIITBBS@users.noreply.github.com>
2025-09-21 14:53:10 +0530

    Commit2

--- backend/middleware/logincheck.js
+++
@@ -1,9 +0,0 @@
-exports.logincheck = (req, res, next) => {
-   // console.log(res.session.loggedIn)
-    //return res.status(401).json({ message: "Not logged in" }); 
-    //console.log("hehe") ;
-  if (!req.session.loggedIn) {
-    return res.status(401).json({ message: "Not logged in" }); 
-  }
-  next();
-};
